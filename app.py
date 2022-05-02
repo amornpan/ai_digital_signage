437582e4ec35786521eadd59585a080efa0f83a0
amornpan <amornpan@gmail.com>
2022-05-02 21:23:06 +0700

    first flask 2nd

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask
 
+# app = Flask(__name__)
+
+UPLOAD_FOLDER = 'static/uploads/'
+
 app = Flask(__name__)
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port='5001', debug=True)
+# @app.route('/')
+# def hello():
+#     return 'Hello, World!'
+#
+# if __name__ == '__main__':
+#     app.run(host='127.0.0.1', port='5001', debug=True)
